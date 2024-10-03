7437bf2d6193f3fe9a31ca23c2968793308560af
Davi Saldanha <davisaldanha.ti@gmail.com>
2024-10-03 11:22:20 -0300

    Second Commit

--- js/main.js
+++ js/main.js
@@ -40,3 +40,83 @@ function salvarTurma(){
         } 
     ).catch(err => console.error(err))
 }
+
+//Função para alimentar os selects para listar alunos e turmas
+function listarDados(){
+
+    axios.get(`${url}/alunos`)
+    .then(
+        (response) => {
+            let alunos = response.data
+            let selectAlunos = document.getElementById('listaAlunos')
+            let html = '<option selected disabled hidden>Escolha uma opção</option>'
+
+            for(let aluno of alunos){
+                html += `<option value="${aluno.id}">${aluno.nome}</option>`
+            }
+
+            selectAlunos.innerHTML = html
+        }
+    ).catch(err => console.error(err))
+
+    axios.get(`${url}/turmas`)
+    .then(
+        (response) => {
+            let turmas = response.data
+            let selectTurmas = document.getElementById('listaTurmas')
+            let html = '<option selected disabled hidden>Escolha uma opção</option>'
+
+            for(let turma of turmas){
+                html += `<option value="${turma.id}">${turma.nome}</option>`
+            }
+
+            selectTurmas.innerHTML = html
+        }
+    ).catch(err => console.error(err))
+}
+
+function matricular(){
+
+    let selectAluno = document.getElementById('listaAlunos')
+    let optionAluno = selectAluno.options[selectAluno.selectedIndex].value
+
+    let selectTurma = document.getElementById('listaTurmas')
+    let optionTurma = selectTurma.options[selectTurma.selectedIndex].value
+
+    const data = {
+        aluno_id: optionAluno,
+        turma_id: optionTurma
+    }
+
+    axios.put(`${url}/matricula`, data)
+    .then(
+        (response) => {
+            alert(response.data.message)
+            window.location.reload()
+        }
+    ).catch(err => console.error(err))
+}
+
+function criarTabela(){
+    axios.get(`${url}/lista`)
+    .then(
+        (response) => {
+            let matriculas =  response.data
+            let table = document.getElementById('table-matriculas')
+            let html = ''
+
+            for(let matricula of matriculas){
+                html += `
+                    <tr>
+                    <td>${matricula.id}</td>
+                    <td>${matricula.aluno}</td>
+                    <td>${matricula.turma}</td>
+                    <td><button class="btn" onclick="">Desmatricular</button></td>
+                    </tr>
+                `
+            }
+
+            table.innerHTML = html
+        }
+    )
+}
